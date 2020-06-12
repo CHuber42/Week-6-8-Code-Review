171374cb48f30c1b52aa96603a6e12b8e603dfb7
Christopher <CHuber42.work@gmail.com>
2020-06-12 11:39:46 -0700

    Attempt #1 at implementing Swagger. Primary edits based classmate's implementation, which in turn was implemented by a tutorial. Primary edits: add app.UseSwagger() directives to Startup.cs, add packagereference in csproj, add using OpenApi.Models directive to Startup.cs

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -18,20 +18,27 @@ namespace AnimalShelter
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-
+             services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("Travel", new OpenApiInfo { Title = "Travel API", Version = "v1" });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
+            app.UseSwagger();
+
+            app.UseSwaggerUI( c=>
+            {
+                c.SwaggerEndpoint("/swagger/Travel/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             if (env.IsDevelopment())
             {
@@ -39,11 +46,9 @@ namespace AnimalShelter
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
